70856c15e21dd61f826abfea34a850c9883ada5d
Shintaro Kimura <kmrshntr@gmail.com>
2014-02-11 16:20:15 +0900

    login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,15 @@
 class SessionsController < ApplicationController
   def create
-    auth = request.env['omniauth.auth']
-    render :text => auth[:credentials][:token]
+    auth = request.env["omniauth.auth"]
+    user = User.find_or_create_by(email:auth['info']['email'])
+    user.name = auth['info']['nickname']
+    user.save
+    session[:user_id] = user.id
+    redirect_to root_path, :notice => "Hello #{user.name}."
   end
 
   def destroy
+	reset_session
+    redirect_to root_path, :notice => "You successfully logged out."
   end
 end
