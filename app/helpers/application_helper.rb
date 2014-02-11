fbe0ac3e547f457584dae0d26849d9cabd49e331
Shintaro Kimura <kmrshntr@gmail.com>
2014-02-11 14:53:49 +0900

    change redirect url

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def signin_with_yammer_path
-    "https://www.yammer.com/dialog/oauth?client_id=#{ENV['YAMMER_KEY']}&redirect_uri=http://stark-atoll-3603.herokuapp.com"
+    "https://www.yammer.com/dialog/oauth?client_id=#{ENV['YAMMER_KEY']}&redirect_uri=http://#{request.domain}"
   end
 end
