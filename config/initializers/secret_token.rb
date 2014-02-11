1e613c9074f838691957f0f29917b909243a620b
Shintaro Kimura <kmrshntr@gmail.com>
2014-02-11 14:39:22 +0900

    generate scaffold event

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CountdownYammer::Application.config.secret_key_base = 'cdaa9edff0ae0ad5dc05381f3b5bbf262c658bdefa8df29c92dd0e4d06055a5a6d0e2d6fb4faa01f913d6137fdfd6535918e86a623db3ad8436e58ebba7f3999'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CountdownYammer::Application.config.session_store :cookie_store, key: '_countdown_yammer_session'
