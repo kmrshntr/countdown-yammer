c56a92e4388d5a680f0389d4967111ca01f50a2c
Shintaro Kimura <kmrshntr@gmail.com>
2014-02-11 15:42:11 +0900

    change login url

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,2 @@
 module ApplicationHelper
-  def signin_with_yammer_path
-    "https://www.yammer.com/dialog/oauth?client_id=#{ENV['YAMMER_KEY']}&redirect_uri=http://#{request.raw_host_with_port}/auth/yammer/callback"
-  end
 end
